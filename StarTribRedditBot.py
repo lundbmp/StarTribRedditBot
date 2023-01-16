69dd6fe82dab7e3b8f6ac5b650c2fc2b89df1d57
lundbmp <m.lundberg008@gmail.com>
2023-01-16 13:48:20 -0600

    loaded urls to a dictionary for faster lookup

--- StarTribRedditBot.py
+++ StarTribRedditBot.py
@@ -5,12 +5,23 @@ reddit = praw.Reddit(client_id=os.environ.get('BOT_CLIENT_ID'),
                      client_secret=os.environ.get('BOT_CLIENT_SECRET'),
                      user_agent='Star Tribune Paywall Bot v1.0 by u/MagicMikeyMike')
 
-subreddit = reddit.subreddit('Minneapolis')
+userComments = {}
+user = reddit.redditor('The_Decoy')
+url = 'https://www.reddit.com/r/CommunismMemes/comments/10d4l7f/all_i_know_is_you_guys_love_the_color_red/'
 
-for post in subreddit.hot(limit=100):
-    hasComment = False
-    if hasattr(post, 'url_overridden_by_dest'):
-        if post.url_overridden_by_dest.lower().__contains__('startribune'):
-            print(post.url_overridden_by_dest)
-            break
+for link in user.comments.new(limit=None):
+    userComments[link.link_permalink] = link.link_permalink
+print("done loading")
 
+if url in userComments.keys():
+    print(userComments[url])
+
+
+# subreddit = reddit.subreddit('Minneapolis')
+#
+# for post in subreddit.hot(limit=100):
+#     hasComment = False
+#     if hasattr(post, 'url_overridden_by_dest'):
+#         if post.url_overridden_by_dest.lower().__contains__('startribune'):
+#             print(post.url_overridden_by_dest)
+#             break
